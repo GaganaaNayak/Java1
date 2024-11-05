d60a00867fff1a9c58511824e0d94cb1241148d5
GaganaaNayak <gagana.23cs039@sode-edu.in>
2024-11-05 16:13:17 +0530

    Update M6.java

--- M6.java
+++ M6.java
@@ -4,7 +4,7 @@ public class M6 {
 
         public static void main(String[] args) {
                 Tools1 t1=new Tools1();
-                System.out.println(t1.reverse("Madan"));
+                System.out.println(t1.reverse("Gagana"));
 
         }
 
